a0200b7d55b4571d6ce9f91cd28dbe2731cba065
MAK TRNKA <maktrnka@comcast.net>
2024-11-25 11:12:56 -0800

    updates

--- src/components/home/HomeBottomPage.jsx
+++ src/components/home/HomeBottomPage.jsx
@@ -42,19 +42,23 @@ const HomeBottomPage = () => {
         <div className="intro-text">
           <span style={{ color: "white", textShadow: "0 4px 8px rgba(0, 0, 0, 10)",}}>
             <p>
-              I'm Mak Trnka, I have recently completed <span style={{ color: "red" }}>CODE FELLOWS</span> learning Software Development for the first time. My enthusiasm lies at the intersection of technology and creativity. Currently, I'm adept in JavaScript, CSS, React, and the MERN stack, with a newfound interest in mastering AWS and TypeScript. Beyond that, I aspire to delve into the realm of Data Science.
+            <span style={{ color: "green", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)"}}>PRE-TECH LIFE</span>: Hi, I'm Mak Trnka! I graduated from the <span style={{ color: "purple" }}>University of Washington</span> in 2022, receiving a BA in Political Science, minoring in International Studies, and later wored for a year as a Behavior Technican. However, my main passion I figured relies on creativity, comparing/contrasting, analyzing and such, so I recently applied myself to the Tech World!
             </p>
             <br />
             <p>
-              Outside the realm of technology, I'm an <span style={{ color: "Plum" }}>AVID TRAVELER</span>, having explored 25 countries to date. My interests span across the world data, history, (geo)politics, diverse cultures, and societal dynamics. Therefore, Traveling will always be one of my major talents and hobbies.
+            <span style={{ color: "grey", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)"}}>BEGINNING TECH</span>: As I've applied myself to the Tech World, I recently completed a Trade/Tech School in July 2024 located in Seattle, named <span style={{ color: "red" }}>CODE FELLOWS</span>, recieving Four Certificates learning Software Development for the first time. Beyond that, I later decided to to delve into the realm of Data Science. Therefore, I have now enrolled myself in a new Trade/Tech School located in NYC, named <span style={{ color: "turquoise" }}>FLATIRON</span>, to begin learning Data Science!
             </p>
             <br />
             <p>
-              I've been described by many as <span style={{ color: "Pink" }}>OPEN-MINDED</span>. In addition to the other topics mentioned, my interest also spans across other topics such as Movies, Pop Culture, Personal Life, Basketball, Wildlife, Food, Breaking News, other Data, and such. I harbor aspirations of starting a podcast to discuss these eclectic topics or even start a YouTube channel.
+            <span style={{ color: "Plum", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)"}}>AVID TRAVELER</span>: Outside the realm of technology, I'm an AVID TRAVELER, having explored 25 countries to date. My interests span across the world data, history, (geo)politics, diverse cultures, and societal dynamics. Therefore, Traveling will always be one of my major talents and hobbies.
             </p>
             <br />
             <p>
-              I'm also a <span style={{ color: "Orange" }}>SERIAL CREATOR</span>. Through my extensive coding skills, I have the ability to design and craft a number of websites. Outside of tech, I possess a knack for ideation and enjoy crafting visually engaging presentations and data-driven reports in tools like Word, Excel, and PowerPoint. What also makes me a serial creator is the fact I have many Business ideas, App ideas, Legislative ideas, and creating content for several different topics (Positive effects come from having ADHD and being on the Spectrum as well as you can see).
+            <span style={{ color: "Pink", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)"}}>OPEN-MINDED</span>: I've been described by many as OPEN-MINDED. In addition to the other topics mentioned, my interest also spans across other topics such as Movies, Pop Culture, Personal Life, Basketball, Wildlife, Food, Breaking News, other Data, and such. I harbor aspirations of starting a podcast to discuss these eclectic topics or even start a YouTube channel.
+            </p>
+            <br />
+            <p>
+            <span style={{ color: "Orange", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)"}}>SERIAL CREATOR</span>: I've also been described as many as a SERIAL CREATOR. Through my extensive coding skills, I have recently developed the ability to craft a number of websites as a Frontend Engineer and also begin analyzing Data as a professional. Outside of tech, I possess a knack for ideation and enjoy crafting visually engaging presentations and data-driven reports in tools like Word, Excel, and PowerPoint. What also makes me a serial creator is the fact I have many Business ideas, App ideas, Legislative ideas, and creating content for several different topics (Positive effects come from having ADHD and being on the Spectrum as well as you can see).
             </p>
             <br />
             <p>
